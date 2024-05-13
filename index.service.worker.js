6ae18acf47b2ba651ad1ba6a7679fc28b54f41e5
plaiddroid <plaiddroid@gmail.com>
2024-05-13 20:56:34 +0530

    fix game controls, logic changes

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1715545395|19202311";
+const CACHE_VERSION = "1715613976|1205907976";
 const CACHE_PREFIX = "Secrets Of The O-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
